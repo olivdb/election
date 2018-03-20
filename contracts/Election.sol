7309c207f10a170c282e600735bc2e7ccf73f280
Olivier van den Biggelaar <ovdbigge@gmail.com>
2018-03-20 21:54:44 +0000

    2 Candidates visible on Client side

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,29 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-    // Store Candidate
-    // Read Candidate
-    string public candidate;
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+
+    //Store Candidates count
+    uint public candidatesCount;
 
     // Constructor
     function Election () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
 }
