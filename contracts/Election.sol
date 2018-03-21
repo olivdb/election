a826aac45efce78098ef2d66860b38c0f7e572f9
Olivier van den Biggelaar <ovdbigge@gmail.com>
2018-03-21 17:42:34 +0000

    3. Cast Votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,11 +8,14 @@ contract Election {
         uint voteCount;
     }
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
     // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
 
-    //Store Candidates count
+    // Store Candidates count
     uint public candidatesCount;
 
     // Constructor
@@ -24,6 +27,20 @@ contract Election {
     function addCandidate (string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote count
+        candidates[_candidateId].voteCount ++;
     }
 
 }
